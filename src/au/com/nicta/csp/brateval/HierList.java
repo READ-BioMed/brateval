8044a47f7845261c6257995784c508df086ccf40
Andrei (Andreas) Scherbakov <ultrasparc@yandex.ru>
2018-08-12 14:59:46 +1000

    Config file reading and using taxonomy in entity summary displaying

---
+++ src/au/com/nicta/csp/brateval/HierList.java
@@ -0,0 +1,45 @@
+package au.com.nicta.csp.brateval;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class HierList<E>
+{
+	private class Elem {
+		int level;
+		E entry;
+		Elem(int l, E e) {entry=e; level=l;}
+	}
+	LinkedList<Elem> elems = new LinkedList<Elem>();
+
+	public interface Visitor<X> {
+		void pre (int currLevel, X curr, X parent);
+		void post(int currLevel, X curr, X parent);
+	}
+
+	void add(int l, E e) {
+		elems.add(new Elem(l, e));
+	}
+
+	Elem traverse(Visitor<E> v, Iterator<Elem> j, Elem e0, E parent) {
+			//if (j < entries.size()) return j;
+			//Visitor a = new Visitor(entities[j], parent);
+			v.pre(e0.level, e0.entry, parent);
+			if (!j.hasNext()) return null;
+			Elem e = j.next();
+			do
+				{
+					if(e.level <= e0.level && parent != null) break;
+					e = traverse(v, j, e, e0.entry);
+				}
+			while (e != null);
+			v.post(e0.level, e0.entry, parent);
+			return e;
+	}
+	
+	public void traverse(Visitor<E> v)
+	{
+		Iterator<Elem> j = elems.iterator();
+		if (j.hasNext())
+				traverse(v, j, j.next(), null);
+	}
+}
---
+++ src/au/com/nicta/csp/brateval/TaxonomyConfig.java
@@ -0,0 +1,121 @@
+package au.com.nicta.csp.brateval;
+import java.lang.Character;
+import java.io.*;
+import java.util.ArrayList;
+
+public class TaxonomyConfig {
+	public class EntityDesc {
+		String name;
+		EntityDesc(String s) {name = s;}
+	}
+
+	public class RelationDesc {
+		String name;
+		// Add args
+		RelationDesc(String s) {name = s;} //Rewrite!
+	}
+	
+	HierList<EntityDesc> entities = new HierList<EntityDesc>();
+	HierList<RelationDesc> relations = new HierList<RelationDesc>();
+	
+	static File findConfigFile() {
+		File fn = new File("./annotation.conf");
+		if (fn.isFile() && fn.canRead()) return fn;
+		return null;
+	}
+		
+	TaxonomyConfig() {
+		readConfigFile(findConfigFile());
+	}
+	
+	private interface EntryAdder {
+		void add(int level, String spec);
+	}
+
+	String consumeSection(BufferedReader r, EntryAdder a)
+		throws IOException {
+		String text;
+		while ((text = r.readLine()) != null) {
+			int level = 0;
+			for (int i = 0; i < text.length() && Character.isWhitespace(text.charAt(i)); ++i)
+			switch (text.charAt(i)) {
+					case ' ': level ++;
+					break;
+					case '\t': level +=4;
+					break;
+					default: level = 0;
+					break;
+			}
+			String s = text.trim();
+			if (!s.isEmpty()) {
+				if (s.charAt(0) == '[')
+					return text; //New section started
+				a.add(level, s);
+			}
+		}
+		return text; //null
+	}
+	
+	void readConfigFile(File file) {
+		if (file == null)	return;
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String text = reader.readLine();
+
+			while (text != null) {
+			  String tt = text.trim();
+			  if (tt.equals("[entities]"))
+			  	text = consumeSection(reader, new EntryAdder() {
+				  	public void add(int l, String s)
+			  		{
+				  		entities.add(l, new EntityDesc(s));
+			  		}});
+			  else if (tt.equals("[relations]"))
+			  	text = consumeSection(reader, new EntryAdder() {
+				  	public void add(int l, String s)
+			  		{
+				  		relations.add(l, new RelationDesc(s));
+			  		}});
+		  	  else if (tt.charAt(0) == '[')
+			  	text = consumeSection(reader, new EntryAdder() {
+				  	public void add(int l, String s) {} });
+			  else
+				text = reader.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+		} catch (IOException e) {
+    		e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null)
+					reader.close();
+			} catch (IOException e) {}
+		}
+	}
+	
+	private ArrayList<String> levelIndents = new ArrayList<String>(16);
+	public String levelIndent(int level) {
+		if (levelIndents.size() <= level ||
+			levelIndents.get(level) == null) {
+				StringBuffer buffer = new StringBuffer();
+				int j = level;
+				while (j >= 4) {buffer.append("\t"); j-=4;}
+				while (j > 0) {buffer.append(" "); j--;}
+				while (levelIndents.size() <= level)
+					levelIndents.add(null);
+				levelIndents.set(level, buffer.toString());
+			}
+		return levelIndents.get(level);
+	}
+	
+	public void traverseEntities(HierList.Visitor<EntityDesc> v) {
+		entities.traverse(v);
+	}
+	public void traverseRelations(HierList.Visitor<RelationDesc> v) {
+		relations.traverse(v);
+	}
+}	
+			
